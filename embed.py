e1edf826f5220df1e583584a715c3cb47ac68fa9
Arjun-A-I <arjunai5768@gmail.com>
2024-02-22 19:12:47 +0530

    OpenAI initialised

--- embed.py
+++ embed.py
@@ -7,10 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-resp = client.embeddings.create(
-  input=["feline friends say"],
-  model="text-embedding-3-small",
-  dimensions=384
-)
 
-# print(resp)
+def get_embeddings(text_to_embed):
+  response = client.embeddings.create(
+    input=[text_to_embed],
+    model="text-embedding-3-small",
+    dimensions=384
+  )
+  embedding = response.data[0].embedding
+  return embedding
+
+print(get_embeddings('hai hello'))
