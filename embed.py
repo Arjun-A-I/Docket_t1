f52a63cdc1eed03d3a9d09664e74d8897643aa95
Arjun-A-I <arjunai5768@gmail.com>
2024-02-24 14:34:04 +0530

    Question upload

--- embed.py
+++ embed.py
@@ -1,5 +1,5 @@
 import pinecone
-from pinecone import Pinecone,PodSpec
+from pinecone import Pinecone,PodSpec,ServerlessSpec
 from openai import OpenAI
 import json
 import os 
@@ -8,23 +8,30 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
+# pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment="gcp-starter")
+pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
+spec = PodSpec(environment="gcp-starter")
+
 def get_embeddings(text_to_embed):
   response = client.embeddings.create(
     input=[text_to_embed],
     model="text-embedding-3-small",
-    dimensions=384
+    dimensions=1536
   )
   embedding = response.data[0].embedding
   return embedding
 
-
-def laod_questions(file):
+def load_questions(file):
   with open(file, 'r') as file:
         questions = json.load(file)
   return questions
 
+index_name = "questions-index"
+index = pc.Index(index_name)
+print(index.describe_index_stats())
 
+questions=load_questions('question.json')
 
-questions=laod_questions('question.json')
-
-print(questions)
+for question in questions:
+    embedding = get_embeddings(question['text'])
+    print(question['text'])   
