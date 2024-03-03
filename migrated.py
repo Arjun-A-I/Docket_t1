f8f5c184100ca50ea8f58e2ab973a981dd63becf
Arjun-A-I <arjunai5768@gmail.com>
2024-03-04 00:26:14 +0530

    Introducing HG model

--- migrated.py
+++ migrated.py
@@ -1,15 +1,31 @@
 import chromadb
+
 chroma_client=chromadb.Client()
+embeddings = HuggingFaceEmbeddings()
+
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+model_name = "BAAI/bge-large-en-v1.5"
+model_kwargs = {'device': 'cuda'}
+encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
+model = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs,
+    query_instruction="为这个句子生成表示以用于检索相关文章："
+)
+model.query_instruction = "为这个句子生成表示以用于检索相关文章："
+
+
 collection=chroma_client.create_collection(name="new_collections")
 
-collection.add(
-    documents=["This is a document","Another doc"],
-    metadatas=[{"source":"mysource"},{"source":"mysource"}],
-    ids=["id1","id2"]
-)
+# collection.add(
+#     documents=["This is a document","Another doc"],
+#     metadatas=[{"source":"mysource"},{"source":"mysource"}],
+#     ids=["id1","id2"]
+# )
 
-results= collection.query(
-    query_texts=["This is a document"],
-    n_results=2
-)
-print(results)
+# results= collection.query(
+#     query_texts=["This is a document but also an extension of the stuffs thats happening here."],
+#     n_results=2
+# )
+# print(results)
